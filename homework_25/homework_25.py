4f73000b376fdbe48b54972f904339c5d01e3c2c
Oleksandr Suligan <suligan.alexander@gmail.com>
2026-04-25 16:20:06 +0200

    added homework 25

---
+++ homework_25/homework_25.py
@@ -0,0 +1,56 @@
+# Деление без ошибок
+# Напишите функцию, которая выполняет деление двух чисел, введенных пользователем, и
+# обрабатывает возможные ошибки.
+# Пример вывода:
+# Введите делимое: 345
+# Введите делитель: 5a
+# Ошибка: Введено некорректное число.
+import logging
+
+
+def division() -> None:
+    try:
+        first_num = float(input("Введите делимое: "))
+        second_num = float(input("Введите делитель: "))
+        result = round(first_num / second_num, 2)
+    except ZeroDivisionError:
+        print("Делитель не может быть 0")
+    except ValueError:
+        print("Некорректное число")
+    else:
+        print(f"Результат: {result}")
+    finally:
+        print("Программа завершена")
+
+
+division()
+
+# Логирование ошибок
+# Перенаправьте в предыдущей задаче вывод ошибок в файл errors.log в соответствии с форматом
+# ниже.
+# Пример вывода:
+# 2025-02-23 22:38:53,686 - ERROR - test.py - 16 - Ошибка: Введено некорректное число.
+
+logging.basicConfig(
+    filename="errors.log",
+    level=logging.ERROR,
+    format="%(asctime)s - %(levelname)s - %(filename)s - %(lineno)d - %(message)s"
+)
+
+
+def division() -> None:
+    try:
+        first_num = float(input("Введите делимое: "))
+        second_num = float(input("Введите делитель: "))
+        result = round(first_num / second_num, 2)
+    except ZeroDivisionError:
+        logging.error("Делитель не может быть 0")
+    except ValueError:
+        logging.error("Некорректное число")
+    else:
+        print(f"Результат: {result}")
+    finally:
+        print("Программа завершена")
+
+
+division()
