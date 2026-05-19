f4992d03b9e204f9bd6a056c9b8e3fb3d201b2e4
Oleksandr Suligan <suligan.alexander@gmail.com>
2026-05-19 16:36:00 +0200

    Changed prints to return in methods

--- homework_37/homework_37.py
+++ homework_37/homework_37.py
@@ -1,5 +1,5 @@
 # Воспроизведение мультимедиа
-# Создайте два класса:
+# создайте два класса:
 # Класс 1
 # AudioFileMixin — требует наличие поля
 # audio_tracks (список треков).
@@ -22,23 +22,29 @@
 # Проверьте работу классов, вызвав методы воспроизведения
 
 class AudioFileMixin:
-    def play_audio(self) -> None:
+    def play_audio(self) -> str:
         if not hasattr(self, "audio_tracks"):
             raise AttributeError("У объекта отсутствует поле audio_tracks")
 
-        print(f"Воспроизведение аудио для {self.__class__.__name__}:")
+        lines = [f"Воспроизведение аудио для {self.__class__.__name__}:"]
+
         for track in self.audio_tracks:
-            print(track)
+            lines.append(track)
+
+        return "\n".join(lines)
 
 
 class VideoFileMixin:
-    def play_video(self) -> None:
+    def play_video(self) -> str:
         if not hasattr(self, "video_files"):
             raise AttributeError("У объекта отсутствует поле video_files")
 
-        print(f"Воспроизведение видео для {self.__class__.__name__}:")
+        lines = [f"Воспроизведение видео для {self.__class__.__name__}:"]
+
         for video in self.video_files:
-            print(video)
+            lines.append(video)
+
+        return "\n".join(lines)
 
 
 class MediaPlayer(AudioFileMixin):
@@ -55,7 +61,6 @@ class Laptop(AudioFileMixin, VideoFileMixin):
 player = MediaPlayer(["track1.mp3", "track2.mp3"])
 laptop = Laptop(["track1.mp3", "track2.mp3"], ["movie.mp4", "trailer.mov"])
 
-player.play_audio()
-
-laptop.play_video()
-laptop.play_audio()
+print(player.play_audio())
+print(laptop.play_video())
+print(laptop.play_audio())
