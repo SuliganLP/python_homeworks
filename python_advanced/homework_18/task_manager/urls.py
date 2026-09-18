8e969846ad30369f2fc7f6c9a51a54b7c7a316cc
Oleksandr Suligan <suligan.alexander@gmail.com>
2026-09-18 16:25:55 +0200

    Add homework 18

---
+++ python_advanced/homework_18/task_manager/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
+from .views import CategoryViewSet
+from .views import (
+    task_statistics,
+    SubTaskListCreateView,
+    SubTaskDetailUpdateDeleteView,
+    TaskDetailUpdateDeleteView,
+    TaskListCreateView
+)
+
+router = DefaultRouter()
+router.register("categories", CategoryViewSet, basename="category")
+urlpatterns = [path("tasks/", TaskListCreateView.as_view(), name="task-list-create"),
+               path("tasks/statistics/", task_statistics, name="task-statistics"),
+               path("tasks/<int:pk>/", TaskDetailUpdateDeleteView.as_view(),
+                    name="task-detail-update-delete"),
+               path("subtasks/", SubTaskListCreateView.as_view(), name="subtask-list-create"),
+               path("subtasks/<int:pk>/", SubTaskDetailUpdateDeleteView.as_view(),
+                    name="subtask-detail-update-delete"),
+               path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+               path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh")
+               ] + router.urls
---
+++ python_advanced/homework_18/task_manager/views.py
@@ -0,0 +1,89 @@
+from rest_framework import status, generics, viewsets
+from rest_framework.decorators import api_view, action
+from rest_framework.response import Response
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsOwnerOrReadOnly
+from django.utils import timezone
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Task, TaskStatus, SubTask, Category
+from .serializers import (
+    SubTaskCreateSerializer,
+    TaskCreateSerializer,
+    TaskDetailSerializer,
+    CategoryCreateSerializer
+)
+
+
+class TaskListCreateView(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskCreateSerializer
+    permission_classes = [IsAuthenticated]
+
+    filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter)
+
+    filterset_fields = ("status", "deadline")
+
+    search_fields = ("title", "description")
+
+    ordering_fields = ("created_at",)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class TaskDetailUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskDetailSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+
+@api_view(["GET"])
+def task_statistics(request):
+    total_tasks = Task.objects.count()
+    task_by_status = {
+        status_value: Task.objects.filter(status=status_value).count()
+        for status_value, _ in TaskStatus.choices
+    }
+    overdue_tasks = Task.objects.filter(deadline__lt=timezone.now()).count()
+
+    data = {
+        "total_tasks": total_tasks,
+        "tasks_by_status": task_by_status,
+        "overdue_tasks": overdue_tasks
+    }
+
+    return Response(data, status=status.HTTP_200_OK)
+
+
+class SubTaskListCreateView(generics.ListCreateAPIView):
+    queryset = SubTask.objects.all()
+    serializer_class = SubTaskCreateSerializer
+
+    filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter)
+
+    filterset_fields = ("status", "deadline")
+
+    search_fields = ("title", "description")
+
+    ordering_fields = ("created_at",)
+
+
+class SubTaskDetailUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = SubTask.objects.all()
+    serializer_class = SubTaskCreateSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategoryCreateSerializer
+
+    @action(detail=True, methods=["get"])
+    def count_tasks(self, request, pk=None):
+        category = self.get_object()
+        count = category.tasks.count()
+
+        return Response({
+            "category": category.name,
+            "tasks_count": count
+        })
